e4ad4502bd2f0711b589c69386a31f110aee35a4
bill <test@qq.com>
2019-12-05 14:02:17 +0800

    add new sample

---
+++ 054 ElementUI-InfiniteScroll/src/App.vue
@@ -0,0 +1,22 @@
+﻿<template>
+<div id="app">
+  <ul class="infinite-list" v-infinite-scroll="load" style="overflow:auto">
+    <li v-for="i in count" class="infinite-list-item">{{ i }}</li>
+  </ul>
+</div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        count: 0
+      }
+    },
+    methods: {
+      load () {
+        this.count += 2
+      }
+    }
+  }
+</script>
